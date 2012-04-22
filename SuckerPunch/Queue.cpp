879c58fe7a144aa887e6cb0445e49a28f4938597
Christian Helmich <kagekirin@gmail.com>
2012-04-22 15:51:02 +0900

    added readme added Queue.h/cpp

---
+++ SuckerPunch/Queue.cpp
@@ -0,0 +1,9 @@
+//
+//  Queue.cpp
+//  SuckerPunch
+//
+//  Created by Christian Helmich on 22.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ SuckerPunch/Queue.h
@@ -0,0 +1,14 @@
+//
+//  Queue.h
+//  SuckerPunch
+//
+//  Created by Christian Helmich on 22.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef SuckerPunch_Queue_h
+#define SuckerPunch_Queue_h
+
+
+
+#endif
