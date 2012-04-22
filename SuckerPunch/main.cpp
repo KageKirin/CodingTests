c5d2673bd90ed6a55d9827dcaed1c1073059d9d0
Christian Helmich <kagekirin@gmail.com>
2012-04-22 15:52:57 +0900

    added test calls and final test output

--- SuckerPunch/main.cpp
+++ SuckerPunch/main.cpp
@@ -3,16 +3,53 @@
 //  SuckerPunch
 //
 //  Created by Christian Helmich on 22.04.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Christian Helmich. All rights reserved.
 //
 
-#include <iostream>
+#include <cstdio>
+#include "Queue.h"
 
 int main(int argc, const char * argv[])
 {
+	printf("Hello, Suckerpunch!\n");
+	
+	//test
+	Q* qtest = create_queue();
+	
+	enqueue_byte(qtest, 0);
+	enqueue_byte(qtest, 1);
+	enqueue_byte(qtest, 2);
+	enqueue_byte(qtest, 3);
 
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	printf("%d\n", dequeue_byte(qtest));
+	printf("%d\n", dequeue_byte(qtest));
+	printf("%d\n", dequeue_byte(qtest));
+	printf("%d\n", dequeue_byte(qtest));
+	
+	destroy_queue(qtest);
+	
+	/*
+	//final output
+	Q* q0 = create_queue();
+	enqueue_byte(q0, 0);
+	enqueue_byte(q0, 1);
+	Q* q1 = create_queue();
+	enqueue_byte(q1, 3);
+	enqueue_byte(q0, 2);
+	enqueue_byte(q1, 4);
+	printf("%d ", dequeue_byte(q0));
+	printf("%d\n", dequeue_byte(q0));
+	enqueue_byte(q0, 5);
+	enqueue_byte(q1, 6);
+	printf("%d ", dequeue_byte(q0));
+	printf("%d\n", dequeue_byte(q0));
+	destroy_queue(q0);
+	printf("%d ", dequeue_byte(q1));
+	printf("%d ", dequeue_byte(q1));
+	printf("%d\n", dequeue_byte(q1));
+	destroy_queue(q1);
+	*/
+	
     return 0;
 }
 
