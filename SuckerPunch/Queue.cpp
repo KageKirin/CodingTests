db1636e30658acc8572eabd12bc3cc6998dd4f2a
Christian Helmich <kagekirin@gmail.com>
2012-04-22 15:52:25 +0900

    added base interface and function stubs

--- SuckerPunch/Queue.cpp
+++ SuckerPunch/Queue.cpp
@@ -3,7 +3,45 @@
 //  SuckerPunch
 //
 //  Created by Christian Helmich on 22.04.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Christian Helmich. All rights reserved.
 //
 
-#include <iostream>
+#include <cstdio>
+
+static unsigned char data[2048];	//queue heap
+
+Q* create_queue()
+{
+}
+
+
+void destroy_queue(Q* q)
+{
+}
+
+
+void enqueue_byte(Q* q, unsigned char b)
+{
+}
+
+
+unsigned char dequeue_byte(Q* q)
+{
+}
+
+
+
+// error handling
+void on_out_of_memory()
+{
+	printf("out of memory\n");
+	while(true)
+	{}
+}
+
+void on_illegal_operation()
+{
+	printf("illegal operation\n");
+	while(true)
+	{}
+}
--- SuckerPunch/Queue.h
+++ SuckerPunch/Queue.h
@@ -3,12 +3,21 @@
 //  SuckerPunch
 //
 //  Created by Christian Helmich on 22.04.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Christian Helmich. All rights reserved.
 //
 
 #ifndef SuckerPunch_Queue_h
 #define SuckerPunch_Queue_h
 
+
+
+Q* create_queue();			//Creates a FIFO byte queue, returning a handle to it.
+void destroy_queue(Q* q);	//Destroy an earlier created byte queue.
+void enqueue_byte(Q* q, unsigned char b);	//Adds a new byte to a queue.
+unsigned char dequeue_byte(Q* q);			//Pops the next byte off the FIFO queue.
+
+void on_out_of_memory();
+void on_illegal_operation();
 
 
 #endif
