34f2ac658384de627c71eb8047bf22b6df57223f
Francisco Pedraza <pacopedraza87@gmail.com>
2020-05-06 17:42:40 -0700

    Adds test structure and gets rid of old main
    Added the test/ structure to place CMakeLists.txt
    as well as test/main.cpp
    Implements Google Test Framework to test the Queue and
    Dequeue, so far only simple test to prove that the framework
    can be used for this example.
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

---
+++ SuckerPunch/SuckerPunch/test/main.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  SuckerPunch
+//
+//  Created by Francisco Pedraza on 06.05.20.
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <gtest/gtest.h>
+#include "../Queue.cpp"
+#include "../stdafx.h"
+
+/* Define constants for tests */
+#define FILL_STRESS 15
+//Q* qtest = create_queue();
+
+class SuckerPunchTest : public ::testing::Test {
+    protected:
+    Q* qtest;
+
+  void SetUp() override {
+      qtest = create_queue();
+  }
+
+  void TearDown() override {
+      destroy_queue(qtest);
+  }
+};
+
+// Positive Test Scenario
+TEST_F(SuckerPunchTest, QueueDequeue) {
+    enqueue_byte(qtest, 0);
+    enqueue_byte(qtest, 1);
+	enqueue_byte(qtest, 2);
+	enqueue_byte(qtest, 3);
+    EXPECT_EQ(dequeue_byte(qtest), 0);
+    EXPECT_EQ(dequeue_byte(qtest), 1);
+    EXPECT_EQ(dequeue_byte(qtest), 2);
+    EXPECT_EQ(dequeue_byte(qtest), 3);
+}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
